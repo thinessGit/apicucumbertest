350725058fbab6ef03025ffd2470ae9ffddfba8a
Thiness Babu <thinessbabu@gmail.com>
2023-07-30 22:46:10 +0530

    Example Api/Service testing in Cucumber added

--- features/sample-api-test.feature
+++ features/sample-api-test.feature
@@ -1,6 +1,10 @@
-Feature: Sample Api/Service testing 
+Feature: Example Api/Service testing in Cucumber
 
-Scenario: Hit any get httpp rest api and validate it 
-Given I have endpoint url available for test 
-When I hit get request
-Then I verify the response  
+    Scenario Outline: Hit any get http rest api and validate it
+        Given I have endpoint "<baseUrl>" available for test
+        When I hit get request for "<userName>"
+        Then I verify the response status 200 and "<userName>" is valid
+
+        Examples:
+            | baseUrl               | userName |
+            | https://api.agify.io/ | Babu     |
--- step-definitions/sample-api-test.js
+++ step-definitions/sample-api-test.js
@@ -1,13 +1,26 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const axios = require("axios");
+const chai = require("chai");
+const expect = chai.expect;
 
-Given("I have endpoint url available for test", function () {
-  console.log("cucumber Given statement");
+let baseUrl;
+let response;
+
+Given("I have endpoint {string} available for test", async function (appUrl) {
+  baseUrl = appUrl;
 });
 
-When("I hit get request", function () {
-  console.log("cucumber When statement");
+When("I hit get request for {string}", async function (userName) {
+  response = await axios.get(`${baseUrl}?name=${userName}`);
+  console.log(response.data);
 });
 
-Then("I verify the response", function () {
-  console.log("cucumber Then statement");
-});
+Then(
+  "I verify the response status {int} and {string} is valid",
+  function (statusCode, userName) {
+    expect(response.data).to.have.property("name");
+    expect(response.data).to.have.property("age");
+    expect(response.data.name).to.be.equals(userName);
+    expect(response.status).to.be.equals(statusCode);
+  }
+);
